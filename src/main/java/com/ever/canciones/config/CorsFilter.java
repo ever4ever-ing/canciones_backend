b1304e63a139be4c81c9577b3adcd0d6b94a1a0f
ever4ever <e.alvarado03@ufromail.cl>
2026-01-14 20:44:35 -0300

    corsFilter

--- src/main/java/com/ever/canciones/config/CorsFilter.java
+++ src/main/java/com/ever/canciones/config/CorsFilter.java
@@ -29,6 +29,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             response.setStatus(HttpServletResponse.SC_OK);
